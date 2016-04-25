497436945dd580b2dc850e614d6117842ab081a8
Andy Miller <rhuk@mac.com>
2016-04-24 19:33:33 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# grav-plugin-star-ratings
+# Star Ratings Plugin
+
+The **Star Ratings** Plugin is for [Grav CMS](http://github.com/getgrav/grav).  This README.md file should be modified to describe the features, installation, configuration, and general usage of this plugin.
+
+## Description
+
+Simple but Powerful Star Ratings plugin for anything!
---
+++ star-ratings.php
@@ -0,0 +1,70 @@
+<?php
+namespace Grav\Plugin;
+
+use Grav\Common\Plugin;
+use RocketTheme\Toolbox\Event\Event;
+
+/**
+ * Class StarRatingsPlugin
+ * @package Grav\Plugin
+ */
+class StarRatingsPlugin extends Plugin
+{
+    /**
+     * @return array
+     *
+     * The getSubscribedEvents() gives the core a list of events
+     *     that the plugin wants to listen to. The key of each
+     *     array section is the event that the plugin listens to
+     *     and the value (in the form of an array) contains the
+     *     callable (or function) as well as the priority. The
+     *     higher the number the higher the priority.
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'onPluginsInitialized' => ['onPluginsInitialized', 0]
+        ];
+    }
+
+    /**
+     * Initialize the plugin
+     */
+    public function onPluginsInitialized()
+    {
+        // Don't proceed if we are in the admin plugin
+        if ($this->isAdmin()) {
+            return;
+        }
+
+        // Enable the main event we are interested in
+        $this->enable([
+            'onTwigInitialized' => ['onTwigInitialized', 0],
+            'onTwigSiteVariables' => ['onTwigSiteVariables', 0],
+        ]);
+    }
+
+    public function onTwigInitialized()
+    {
+        $this->grav['twig']->twig()->addFunction(
+            new \Twig_SimpleFunction('stars', [$this, 'generateStars'])
+        );
+    }
+
+    public function onTwigSiteVariables()
+    {
+        if ($this->config->get('plugins.star-ratings.built_in_css')) {
+            $this->grav['assets']
+                ->addCss('plugin://star-ratings/css/star-ratings.css');
+        }
+
+        $this->grav['assets']
+            ->add('jquery', 101)
+            ->addJs('plugin://star-ratings/js/jquery.star-rating-svg.min.js');
+    }
+
+    public function generateStars($id, $num_stars=5, $star_width=16)
+    {
+
+    }
+}
